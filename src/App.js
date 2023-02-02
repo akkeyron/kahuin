15a0181041bd1802e28d08cfa764a9451af12625
loqman <loqmanhakim74@gmail.com>
2023-02-02 13:47:54 +0800

    vertical flip effect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useMediaQuery } from 'react-responsive';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Invite from './components/Invite/Invite';
 import Rsvp from './components/Rsvp/Rsvp';
 import Guestbook from './components/Guestbook/Guestbook';
@@ -11,41 +12,29 @@ import Map from './components/Map/Map';
 const pages = [
   <>
     <div class="front">
-      <div id="f1" class="front-content">
-        <Header />
-      </div>
-    </div>
-    <div class="back">
-      <div id="b1" class="back-content">
-        <Invite />
-      </div>
+      <Header />
     </div>
   </>,
   <>
     <div class="front">
-      <div id="f2" class="front-content">
-        <Map />
-      </div>
-    </div>
-    <div class="back">
-      <div id="b2" class="back-content">
-        <Rsvp />
-      </div>
+      <Invite />
     </div>
   </>,
   <>
     <div class="front">
-      <div id="f3" class="front-content">
-        <Guestbook />
-      </div>
-    </div>
-    <div class="back">
-      <div id="b3" class="back-content">
-        <h1>Back 3</h1>
-      </div>
+      <Map />
     </div>
   </>,
 ];
+
+const pages2 = [
+  { id: 1, component: <><div className='front'><Header /></div></> },
+  { id: 2, component: <><div className='front'><Invite /></div></> },
+  { id: 3, component: <><div className='front'><Map /></div></> },
+  { id: 4, component: <><div className='front'><Rsvp /></div></> },
+  { id: 5, component: <><div className='front'><Guestbook /></div></> },
+  { id: 6, component: <><div className='front'><Footer /></div></> },
+]
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -57,10 +46,11 @@ const App = () => {
   const paper1Ref = useRef(null);
   const paper2Ref = useRef(null);
   const paper3Ref = useRef(null);
+  const paperRef = useRef([]);
 
   const divref = useRef(null);
 
-  const maxLocation = pages.length * 2;
+  const maxLocation = pages2.length;
   // if mobile screen
   const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
 
@@ -75,154 +65,108 @@ const App = () => {
     };
   }, []);
 
-  useEffect(() => {
-    if (isMobile) {
-      //screen size is mobile
-      // if even page
-      if (currentPage % 2 === 0) {
-        bookRef.current.style.transform = "translateX(100%)";
-      } else {
-        bookRef.current.style.transform = "translateX(0%)";
-      }
-    } else {
-      //screen size is not mobile
-      bookRef.current.classList.remove("even-page");
-    }
-
-  }, [currentPage])
-
   function handleNext() {
     setCurrentPage((prevPage) => Math.min(prevPage + 1, maxLocation));
+    paperRef.current[currentPage - 1].classList.add("flipped");
+    paperRef.current[currentPage - 1].style.zIndex = 2;
+    paperRef.current[currentPage].style.zIndex = 1;
+
     // handle next button for desktop and table screen
-    if (currentPage < maxLocation && !isMobile) {
-      switch (currentPage) {
-        case 1:
-          openBook();
-          paper1Ref.current.classList.add("flipped");
-          paper1Ref.current.style.zIndex = 2;
-          paper2Ref.current.style.zIndex = 1;
-          break;
-        case 2:
-          paper2Ref.current.classList.add("flipped");
-          paper1Ref.current.style.zIndex = 0;
-          paper2Ref.current.style.zIndex = 2;
-          paper3Ref.current.style.zIndex = 1;
-          break;
-        case 3:
-          paper3Ref.current.classList.add("flipped");
-          paper2Ref.current.style.zIndex = 1;
-          paper3Ref.current.style.zIndex = 2;
-          closeBook(false);
-          break;
-        default:
-          throw new Error("unkown state");
-      }
-      // handle next button for mobile screen
-    } else if (currentPage < maxLocation && isMobile) {
-      switch (currentPage) {
-        case 1:
-          paper1Ref.current.classList.add("flipped");
-          paper1Ref.current.style.zIndex = 2;
-          paper2Ref.current.style.zIndex = 1;
-          break;
-        case 3:
-          paper2Ref.current.classList.add("flipped");
-          paper1Ref.current.style.zIndex = 0;
-          paper2Ref.current.style.zIndex = 2;
-          paper3Ref.current.style.zIndex = 1;
-          break;
-        case 5:
-          paper3Ref.current.classList.add("flipped");
-          paper2Ref.current.style.zIndex = 1;
-          paper3Ref.current.style.zIndex = 2;
-          break;
-        default:
-          throw new Error("unkown state");
-      }
-    }
+    // if (currentPage < maxLocation) {
+    //   switch (true) {
+    //     // front page
+    //     case (currentPage === 1):
+    //       paperRef.current[currentPage-1].classList.add("flipped");
+    //       paperRef.current[currentPage-1].style.zIndex = 2;
+    //       paperRef.current[currentPage].style.zIndex = 1;
+    //       break;
+    //       // middle page
+    //     case (currentPage > 1 && currentPage < pages2.length):
+    //       console.log("page 2");
+    //       paperRef.current[currentPage-1].classList.add("flipped");
+    //       paperRef.current[currentPage-2].style.zIndex = 1;
+    //       paperRef.current[currentPage-1].style.zIndex = 2;
+    //       paperRef.current[currentPage].style.zIndex = 1;
+    //       break;
+    //     case (currentPage === pages2.length):
+    //       paperRef.current[currentPage-1].classList.add("flipped");
+    //       paperRef.current[currentPage-2].style.zIndex = 1;
+    //       paperRef.current[currentPage-1].style.zIndex = 2;
+    //       break;
+    //     default:
+    //       throw new Error("unkown state");
+    //   }
+    // }
   }
   function handlePrev() {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-    if (currentPage > 1 && !isMobile) {
-      switch (currentPage) {
-        case 2:
-          closeBook(true);
-          paper1Ref.current.classList.remove("flipped");
-          paper1Ref.current.style.zIndex = 2;
-          paper2Ref.current.style.zIndex = 1;
-          break;
-        case 3:
-          paper2Ref.current.classList.remove("flipped");
-          paper1Ref.current.style.zIndex = 1;
-          paper2Ref.current.style.zIndex = 2;
-          paper3Ref.current.style.zIndex = 0;
-          break;
-        case 4:
-          openBook();
-          paper3Ref.current.classList.remove("flipped");
-          paper2Ref.current.style.zIndex = 1;
-          paper3Ref.current.style.zIndex = 2;
-          break;
-        default:
-          throw new Error("unkown state");
-      }
-    } else if (currentPage > 1 && isMobile) {
-      switch (currentPage) {
-        case 2:
-          paper1Ref.current.classList.remove("flipped");
-          paper1Ref.current.style.zIndex = 2;
-          paper2Ref.current.style.zIndex = 1;
-          break;
-        case 4:
-          paper2Ref.current.classList.remove("flipped");
-          paper1Ref.current.style.zIndex = 1;
-          paper2Ref.current.style.zIndex = 2;
-          paper3Ref.current.style.zIndex = 0;
-          break;
-        case 6:
-          paper3Ref.current.classList.remove("flipped");
-          paper2Ref.current.style.zIndex = 1;
-          paper3Ref.current.style.zIndex = 2;
-          break;
-        default:
-          throw new Error("unkown state");
-      }
-    }
-  }
-  function openBook() {
-    // move the book 
-    if (!isMobile) {
-      bookRef.current.style.transform = "translateX(50%)";
+    if (currentPage > 1) {
+      paperRef.current[currentPage - 2].classList.remove("flipped");
+      paperRef.current[currentPage - 2].style.zIndex = 2;
+      paperRef.current[currentPage - 1].style.zIndex = 1;
+      paperRef.current[currentPage].style.zIndex = 0;
     }
 
-    // prevBtnRef.current.style.transform = "translateX(-180px)";
-    // nextBtnRef.current.style.transform = "translateX(180px)";
+
+
+    // if (currentPage > 1) {
+    //   switch (currentPage) {
+    //     case 2:
+    //       paper1Ref.current.classList.remove("flipped");
+    //       paper1Ref.current.style.zIndex = 2;
+    //       paper2Ref.current.style.zIndex = 1;
+    //       break;
+    //     case 3:
+    //       paper2Ref.current.classList.remove("flipped");
+    //       paper1Ref.current.style.zIndex = 1;
+    //       paper2Ref.current.style.zIndex = 2;
+    //       paper3Ref.current.style.zIndex = 0;
+    //       break;
+    //     case 4:
+    //       paper3Ref.current.classList.remove("flipped");
+    //       paper2Ref.current.style.zIndex = 1;
+    //       paper3Ref.current.style.zIndex = 2;
+    //       break;
+    //     default:
+    //       throw new Error("unkown state");
+    //   }
+    // }
   }
 
-  function closeBook(isAtBeginning) {
-    if (isAtBeginning) {
-      bookRef.current.style.transform = "translateX(0%)";
-    } else {
-      bookRef.current.style.transform = "translateX(100%)";
-    }
-    prevBtnRef.current.style.transform = "translateX(0px)";
-    nextBtnRef.current.style.transform = "translateX(0px)";
+  // return (
+  //   <React.Fragment>
+  //     <div className='kad-kahwin' >
+  //       <div ref={bookRef} id="book" style={{ height: height }}>
+  //         <div ref={paper1Ref} id="p1" className="paper">
+  //           {pages[0]}
+  //         </div>
+  //         <div ref={paper2Ref} id="p2" className="paper">
+  //           {pages[1]}
+  //         </div>
+  //         <div ref={paper3Ref} id="p3" className="paper">
+  //           {pages[2]}
+  //         </div>
+  //       </div>
+  //       <div className='nav-button'>
+  //         {currentPage === 1 ? "" : <button type='button' ref={prevBtnRef} id="prev-btn" onClick={handlePrev}>Prev</button>}
+  //         {currentPage === maxLocation ? "" : <button type='button' ref={nextBtnRef} id="next-btn" onClick={handleNext}>Next</button>}
+  //       </div>
 
-  }
+
+  //     </div>
+  //   </React.Fragment>
+  // )
 
   return (
     <React.Fragment>
       <div className='kad-kahwin' >
         <div ref={bookRef} id="book" style={{ height: height }}>
-          <div ref={paper1Ref} id="p1" className="paper">
-            {pages[0]}
-          </div>
-          <div ref={paper2Ref} id="p2" className="paper">
-            {pages[1]}
-          </div>
-          <div ref={paper3Ref} id="p3" className="paper">
-            {pages[2]}
-          </div>
+
+          {pages2.map((page, idx) => (
+            <div key={page.id} id={`p${page.id}`} className="paper" ref={(el) => (paperRef.current[idx] = el)}>
+              {page.component}
+            </div>
+          ))}
         </div>
         <div className='nav-button'>
           {currentPage === 1 ? "" : <button type='button' ref={prevBtnRef} id="prev-btn" onClick={handlePrev}>Prev</button>}
@@ -231,7 +175,7 @@ const App = () => {
 
 
       </div>
-    </React.Fragment>
+    </React.Fragment >
   )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import './Header.css'
-import IMG from '../../assets/header.PNG'
-import TITLE from '../../assets/walimah.PNG'
+import INITIAL from '../../assets/initial.PNG'
+import WALIMAH from '../../assets/walimah_khat.png'
+import ROMAN_WALIMAH from '../../assets/roman_walimah.PNG'
+import NAME_DATE from '../../assets/name_date.PNG'
+import DAY_DATE from '../../assets/day_date.PNG'
 
 const Header = () => {
   return (
     <header className='main__header'>
       <div className='main__header-title'>
-        <img src={TITLE} />
-
+        <img src={WALIMAH} />
       </div>
       <div className='main__header-pengantin'>
-        
+        <img src={ROMAN_WALIMAH} />
+      </div>
+      <div className='main__header-pengantin'>
+        <img src={INITIAL} />
       </div>
 
       <div className='main__header-venue'>
+        <img src={NAME_DATE} />
 
+      </div>
+      <div className='main__header-pengantin'>
+        <img src={DAY_DATE} />
       </div>
     </header>
   )
