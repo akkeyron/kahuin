dbd39bc9c14cec3770c3a48c838179b8889a2a75
loqman <loqmanhakim74@gmail.com>
2023-02-04 22:29:04 +0800

    page1 done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import './Header.css'
-import INITIAL from '../../assets/initial.PNG'
+import INITIAL from '../../assets/initial.png'
 import WALIMAH from '../../assets/walimah_khat.png'
-import ROMAN_WALIMAH from '../../assets/roman_walimah.PNG'
-import NAME_DATE from '../../assets/name_date.PNG'
-import DAY_DATE from '../../assets/day_date.PNG'
+import DAY_DATE from '../../assets/day_date.png'
 
 const Header = () => {
   return (
-    <header className='main__header'>
+    <div className='main__header'>
       <div className='main__header-title'>
-        <img src={WALIMAH} />
+        <img src={WALIMAH} alt="" />
       </div>
       <div className='main__header-pengantin'>
-        <img src={ROMAN_WALIMAH} />
-      </div>
-      <div className='main__header-pengantin'>
-        <img src={INITIAL} />
+        <img src={INITIAL} alt="" />
       </div>
 
       <div className='main__header-venue'>
-        <img src={NAME_DATE} />
-
+        <img src={DAY_DATE} alt="" />
       </div>
-      <div className='main__header-pengantin'>
-        <img src={DAY_DATE} />
-      </div>
-    </header>
+    </div>
   )
 }
 
--- src/components/Invite/Invite.js
+++ src/components/Invite/Invite.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import './Invite.css'
 
+// assets
+import BISMILLAH from '../../assets/page2/bismillah.png'
+import MIRA_LOQMAN from '../../assets/page2/mira_loqman.png'
+import GELARAN from '../../assets/page2/gelaran.png'
+import PARENTS from '../../assets/page2/mak_abah.png'
+import SYUKUR from '../../assets/page2/syukur.png'
+
 const Invite = () => {
     return (
-        <div id="invite">
-            <div className='container invite-container'>
-                <div className='invite-text'>
-                    <h4>Sini tulis nama mak abah, nama pengantin</h4>
-
-                    <h4>
-                        Menjemput tan sri puan atok nenek saudara mara abang akak
-                    </h4>
-                </div>
-
-
+        <div className='invite-container'>
+            <div className='invite__assets-bismillah'>
+                <img src={BISMILLAH} alt="" />
+            </div>
+            <div className='invite__assets-syukur'>
+                <img src={SYUKUR} alt="" />
+            </div>
+            <div className='invite__assets-parents'>
+                <img src={PARENTS} alt="" />
+            </div>
+            <div className='invite__assets-gelaran'>
+                <img src={GELARAN} alt="" />
+            </div>
+            <div className='invite__assets-pengantin'>
+                <img src={MIRA_LOQMAN} alt="" />
             </div>
         </div>
 
