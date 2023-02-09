4386c623db7dce15ae69a143b22b13ff693e19ff
loqman <loqmanhakim74@gmail.com>
2023-02-09 23:38:52 +0800

    Add page 6, and petal flower effect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import './Header.css'
 import INITIAL from '../../assets/initial.png'
 import WALIMAH from '../../assets/walimah_khat.png'
 import DAY_DATE from '../../assets/day_date.png'
+import FLOWERS from '../../assets/page1/petal.GIF'
 
 const Header = () => {
   return (
@@ -12,6 +13,7 @@ const Header = () => {
       </div>
       <div className='main__header-pengantin'>
         <img src={INITIAL} alt="" />
+        <img src={FLOWERS} alt="" className='petals' />
       </div>
 
       <div className='main__header-venue'>
