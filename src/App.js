9207204f9eeb967b7808d7b4d81a17b143b499f1
loqman <loqmanhakim74@gmail.com>
2023-02-19 15:36:59 +0800

    revamp navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -6,11 +6,7 @@ import Invite from './components/Invite/Invite';
 import Rsvp from './components/Rsvp/Rsvp';
 import Guestbook from './components/Guestbook/Guestbook';
 import Map from './components/Map/Map';
-
-// nav icons
-import { TfiArrowCircleDown, TfiArrowCircleUp } from 'react-icons/tfi'
-import { MdCall, MdLocationOn, MdHome, MdRsvp } from 'react-icons/md'
-import { SlVolume2, SlVolumeOff } from 'react-icons/sl'
+import Navbar from './components/Navbar/Navbar'
 
 const pages = [
   { id: 1, component: <><Header /></> },
@@ -22,51 +18,11 @@ const pages = [
 ]
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [height, setHeight] = useState(0);
-  const [volume, setVolume] = useState(true);
-
   const bookRef = useRef(null);
-  const prevBtnRef = useRef(null);
-  const nextBtnRef = useRef(null);
   const paperRef = useRef([]);
 
-  const maxLocation = pages.length;
-
-  // useEffect(() => {
-  //   paperRef
-  // }, []);
-
-  // useEffect(() => {
-  //   const calculateHeight = () => {
-  //     setHeight(window.innerHeight - 35);
-  //   };
-  //   calculateHeight();
-  //   window.addEventListener("resize", calculateHeight);
-  //   return () => {
-  //     window.removeEventListener("resize", calculateHeight);
-  //   };
-  // }, []);
-
-  function handleNext() {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, maxLocation));
-    paperRef.current[currentPage - 1].classList.add("flipped");
-    paperRef.current[currentPage - 1].style.zIndex = 2;
-    paperRef.current[currentPage].style.zIndex = 1;
-  }
-
-  function handlePrev() {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-    if (currentPage > 1) {
-      paperRef.current[currentPage - 2].classList.remove("flipped");
-      paperRef.current[currentPage - 2].style.zIndex = 2;
-      paperRef.current[currentPage - 1].style.zIndex = 1;
-      paperRef.current[currentPage].style.zIndex = 0;
-    }
-  }
-
   return (
-    <div className='kad-kahwin' >
+    <div className='kad-kahwin'>
       <div ref={bookRef} id="book" style={{ height: (window.innerHeight - 35) }}>
 
         {pages.map((page, idx) => (
@@ -75,11 +31,11 @@ const App = () => {
           </div>
         ))}
       </div>
-      <div className='nav-button'>
-        <div onClick={() => {setVolume(!volume)}} className="nav-btn">{volume === true ? <SlVolume2 size={28} /> : <SlVolumeOff size={28} />}</div>
-        {currentPage === 1 ? "" : <div ref={prevBtnRef} className="nav-btn" onClick={handlePrev}><TfiArrowCircleUp size={28} /></div>}
-        {currentPage === maxLocation ? "" : <div ref={nextBtnRef} className="nav-btn" onClick={handleNext}><TfiArrowCircleDown size={28} /></div>}
-      </div>
+      {/* pass ref to navbar */}
+      {/* the value is undefined in navbar component after passing*/}
+
+      <Navbar paperRef={paperRef} maxPage={pages.length - 1} />
+
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,122 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { MdCall, MdLocationOn, MdHome, MdRsvp } from 'react-icons/md'
+import { TfiLocationPin } from 'react-icons/tfi'
 import './Navbar.css'
 // nav icons
 import { TfiArrowCircleDown, TfiArrowCircleUp } from 'react-icons/tfi'
+import { SlVolume2, SlVolumeOff } from 'react-icons/sl'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { SlEnvolope } from 'react-icons/sl'
+import { HiOutlinePencilSquare } from 'react-icons/hi2'
 
-const Navbar = (endPage) => {
-  const [activeNav, setActiveNav] = useState('#');
+
+const Navbar = (props) => {
+  const [volume, setVolume] = useState(true);
+  const currentPage = useRef(0);
+  const [menuPopup, setMenuPopup] = useState(false);
+  const paperRef = props.paperRef;
+  const dropdownRef = useRef(null);
+  const maxLocation = props.maxPage;
+
+  // if user clicked outside of the dropdown close it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setMenuPopup(false);
+      }
+    };
+    
+    // add event listener to close dropdown when click outside
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // cleanup
+      console.log('cleanup');
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+
+  function handleNext() {
+    if (currentPage.current >= maxLocation) return;
+
+
+    paperRef.current[currentPage.current].classList.add("flipped");
+    paperRef.current[currentPage.current].style.zIndex = 2;
+    paperRef.current[currentPage.current + 1].style.zIndex = 1;
+
+    currentPage.current = currentPage.current + 1;
+    console.log('current page: ', currentPage.current);
+  }
+
+  function handlePrev() {
+    if (currentPage.current === 0) return;
+
+    paperRef.current[currentPage.current - 1].classList.remove("flipped");
+    paperRef.current[currentPage.current - 1].style.zIndex = 2;
+    paperRef.current[currentPage.current].style.zIndex = 1;
+
+    // handle error when it is the last page
+    if (currentPage.current !== maxLocation) {
+      paperRef.current[currentPage.current + 1].style.zIndex = 0;
+    }
+
+
+    currentPage.current = currentPage.current - 1;
+    console.log('current page: ', currentPage.current);
+  }
+
+  // make a jump to page number function
+  function jumpToPage(pageNumber) {
+    if (pageNumber > currentPage.current) {
+      for (let i = currentPage.current; i < pageNumber; i++) {
+        paperRef.current[i].classList.add("flipped");
+        paperRef.current[i].style.zIndex = 2;
+        paperRef.current[i + 1].style.zIndex = 1;
+      }
+    }
+
+    else if (pageNumber < currentPage.current) {
+      for (let i = currentPage.current; i > pageNumber; i--) {
+        paperRef.current[i - 1].classList.remove("flipped");
+        paperRef.current[i - 1].style.zIndex = 2;
+        paperRef.current[i].style.zIndex = 1;
+
+        // handle error when its last page
+        if (i !== maxLocation) {
+          paperRef.current[i + 1].style.zIndex = 0;
+        }
+      }
+    }
+
+    currentPage.current = pageNumber;
+    console.log('current page: ', currentPage.current);
+  }
+
+  console.log('menuPopup: ', menuPopup);
+
   return (
-    <nav>
-      <div href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><TfiArrowCircleDown /></div>
-      <div href="#rsvp" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><TfiArrowCircleUp /></div>
+    <nav className='nav-button'>
+      <div onClick={() => { setVolume(!volume) }} className="nav-btn">{volume === true ? <SlVolume2 size={28} /> : <SlVolumeOff size={28} />}</div>
+      <div className="nav-btn" onClick={handlePrev}><TfiArrowCircleUp size={28} /></div>
+      <div className="nav-btn" onClick={handleNext}><TfiArrowCircleDown size={28} /></div>
+      <div className="nav-btn"><RxHamburgerMenu size={28}  onClick={() => setMenuPopup(!menuPopup)}/></div>
+      <div className={`dropdown-content ${menuPopup ? 'active' : ''}`} ref={dropdownRef}>
+        <ul>
+          <li onClick={() => jumpToPage(2)}>
+            <div className='dropdown__content-icon'><TfiLocationPin size={25} /></div>
+            <p>MAP</p>
+          </li>
+          <li onClick={() => jumpToPage(3)}>
+            <div className='dropdown__content-icon'><SlEnvolope size={25} /></div>
+            <p>RSVP</p>
+          </li>
+          <li onClick={() => jumpToPage(4)}>
+            <div className='dropdown__content-icon'><HiOutlinePencilSquare size={25} /></div>
+            <p>WISH</p>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
