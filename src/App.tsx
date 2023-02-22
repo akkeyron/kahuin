ec20ae2a01e692e18b7cfa52edcb755cbd133436
loqman <loqmanhakim74@gmail.com>
2023-02-22 13:33:34 +0800

    Added typescript, fixed dropdown

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import React, { useRef, useState } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Invite from './components/Invite/Invite';
+import Rsvp from './components/Rsvp/Rsvp';
+import Guestbook from './components/Guestbook/Guestbook';
+import Map from './components/Map/Map';
+import Navbar from './components/Navbar/Navbar';
+
+// import music from './assets/lovers_oath.ogg';
+
+const pages = [
+  { id: 1, component: <><Header /></> },
+  { id: 2, component: <><Invite /></> },
+  { id: 3, component: <><Map /></> },
+  { id: 4, component: <><Rsvp /></> },
+  { id: 5, component: <><Guestbook /></> },
+  { id: 6, component: <><Footer /></> },
+];
+
+const App = () => {
+  // states
+  const [muted, setMuted] = useState(false);
+
+  // refs
+  const paperRef = useRef<Array<HTMLDivElement>>([]);
+  // const play = music;
+  const maxLocation: number = pages.length - 1;
+
+  // refs
+  const currentPage = useRef<number>(0);
+
+  // when the user click next button
+  const handleNext = () => {
+    if (currentPage.current >= maxLocation) return;
+    if (paperRef.current.length <= currentPage.current) return;
+
+    paperRef.current[currentPage.current].classList.add("flipped");
+    paperRef.current[currentPage.current].style.zIndex = '2';
+    paperRef.current[currentPage.current + 1].style.zIndex = '1';
+
+    currentPage.current = currentPage.current + 1;
+  }
+
+  // when the user click prev button
+  const handlePrev = () => {
+    if (currentPage.current === 0) return;
+
+    paperRef.current[currentPage.current - 1].classList.remove("flipped");
+    paperRef.current[currentPage.current - 1].style.zIndex = '2';
+    paperRef.current[currentPage.current].style.zIndex = '1';
+
+    // handle error when it is the last page
+    if (currentPage.current !== maxLocation) {
+      paperRef.current[currentPage.current + 1].style.zIndex = '0';
+    }
+
+    currentPage.current = currentPage.current - 1;
+  }
+
+  // jump to page
+  const jumpToPage = (pageNumber: number) => {
+    if (pageNumber > currentPage.current) {
+      for (let i = currentPage.current; i < pageNumber; i++) {
+        paperRef.current[i].classList.add("flipped");
+        paperRef.current[i].style.zIndex = '2';
+        paperRef.current[i + 1].style.zIndex = '1';
+      }
+    }
+
+    else if (pageNumber < currentPage.current) {
+      for (let i = currentPage.current; i > pageNumber; i--) {
+        paperRef.current[i - 1].classList.remove("flipped");
+        paperRef.current[i - 1].style.zIndex = '2';
+        paperRef.current[i].style.zIndex = '1';
+
+        // handle error when its last page
+        if (i !== maxLocation) {
+          paperRef.current[i + 1].style.zIndex = '0';
+        }
+      }
+    }
+
+    currentPage.current = pageNumber;
+  }
+
+
+
+  return (
+    <div className='kad-kahwin'>
+      {/* <audio src={play} autoPlay={true} loop={true} controls={false} /> */}
+      <div id="book">
+        {paperRef.current = new Array(pages.length)}
+
+        {pages.map((page, idx) => (
+          <div key={page.id} id={`p${page.id}`} className="paper" ref={(el) => {
+            if (el) {
+              paperRef.current[idx] = el;
+            }
+          }}>
+            {page.component}
+          </div>
+        ))}
+      </div>
+      {/* pass ref to navbar */}
+      {/* the value is undefined in navbar component after passing*/}
+
+      <Navbar handlePrev={handlePrev} handleNext={handleNext} jumpToPage={jumpToPage} />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { useState, useRef, useEffect, FC } from 'react';
+import './Navbar.css'
+
+// nav icons
+import { TfiArrowCircleDown, TfiArrowCircleUp, TfiLocationPin } from 'react-icons/tfi'
+import { SlVolume2, SlVolumeOff } from 'react-icons/sl'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { SlEnvolope } from 'react-icons/sl'
+import { HiOutlinePencilSquare } from 'react-icons/hi2'
+
+// music
+import music from '../../assets/music.ogg';
+
+// if user clicked outside of the dropdown close it
+const useOnClickOutside = (dropdownRef: React.RefObject<HTMLDivElement>, handler: React.MouseEventHandler<HTMLButtonElement>, menuRef:React.RefObject<HTMLDivElement>) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!dropdownRef.current || dropdownRef.current.contains(event.target) || menuRef.current?.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+
+  }, [dropdownRef, handler]);
+}
+
+// interface
+interface IProps {
+  handlePrev: () => void;
+  handleNext: () => void;
+  jumpToPage: (page: number) => void;
+}
+
+const Navbar: FC<IProps> = ({ handlePrev, handleNext, jumpToPage }) => {
+  // React states
+  const [menuPopup, setMenuPopup] = useState(false);
+  const [muted, setMuted] = useState(false);
+
+  // React refs
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside(dropdownRef, () => setMenuPopup(false), menuRef);
+
+  return (
+    <nav className='nav-button'>
+      {/* music player */}
+      <div onClick={() => setMuted((prev) => !prev)} className="nav-btn">{muted === false ? <SlVolume2 size={28} /> : <SlVolumeOff size={28} />}</div>
+      <audio src={music} autoPlay={true} loop={true} muted={muted} controls={false} />
+
+      {/* navigation buttons */}
+      <div className="nav-btn" onClick={handlePrev}><TfiArrowCircleUp size={28} /></div>
+      <div className="nav-btn" onClick={handleNext}><TfiArrowCircleDown size={28} /></div>
+      <div className="nav-btn" ref={menuRef}><RxHamburgerMenu size={28} onClick={(event) => setMenuPopup(() => {
+        event.stopPropagation();
+        return !menuPopup;
+      })} /></div>
+
+      {/* dropdown menu */}
+      <div className={`dropdown-content ${menuPopup ? 'active' : ''}`} ref={dropdownRef}>
+        <ul>
+          <li onClick={() => jumpToPage(2)}>
+            <div className='dropdown__content-icon'><TfiLocationPin size={23} /></div>
+            <p>MAP</p>
+          </li>
+          <li onClick={() => jumpToPage(3)}>
+            <div className='dropdown__content-icon'><SlEnvolope size={23} /></div>
+            <p>RSVP</p>
+          </li>
+          <li onClick={() => jumpToPage(4)}>
+            <div className='dropdown__content-icon'><HiOutlinePencilSquare size={23} /></div>
+            <p>WISH</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
